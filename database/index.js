892e25ccf7683fc81466227d425e85a68e32baf1
Adryel Simon <adryel@live.it>
2022-04-19 21:21:20 -0300

    commit

---
+++ database/index.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+
+const DB_NAME = "movies";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_CONFIG = {
+  dialect: "mysql",
+  host: "localhost",
+  port: 3306,
+};
+
+// objeto para guardar a conexão do banco dados
+let db = {};
+
+try {
+  db = new Sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIG);
+} catch (error) {
+  console.error("Error ao tentar uma conexão com banco dados");
+}
+
+// db.hasconnection = this.hasconnection
+
+async function hasConection() {
+  try {
+    await db.authenticate();
+    console.log("Banco dados conectado!");
+  } catch (error) {
+    console.error("Erro ao tentar se conectar ao banco de dados");
+  }
+}
+
+Object.assign(db, {
+  hasConection,
+});
+
+module.exports = db;
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const GenderController = require("../controllers/gender.controller");
+const routes = express.Router();
+
+routes.get("/gender", GenderController.listar);
+
+module.exports = routes;
